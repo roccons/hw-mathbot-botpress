f2de74551985ea7fb853283d579fa81a2cbb656f
Marco Antonio Vazquez Alonso <mrc_ant_18@hotmail.com>
2018-08-13 13:26:25 -0500

    Variations of answers working

--- src/renderers.js
+++ src/renderers.js
@@ -1,1 +1,12 @@
-module.exports = {}
+const _ = require('lodash')
+
+/**
+ * This is necesary if you want to alternate answers
+ */
+module.exports = {
+    builtin_text: data => {
+      const text = _.sample([data.text, ...(data.variations || [])])
+  
+        return { text: text, typing: !!data.typing }
+      },
+  }
